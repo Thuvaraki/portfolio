a038dd0d0f8217f96175e903190a5a9e77387423
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2025-06-24 03:06:14 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes.js";
+import Navbar from "./components/Navbar";
+import Hero from "./components/HeroSection";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 
 function App() {
-  return <div className="App">Portfolio</div>;
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal);
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Navbar />
+        <Body>
+          <Hero />
+          <Wrapper>
+            <Education />
+            <Skills />
+            <Experience />
+          </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Contact />
+          </Wrapper>
+          <Footer />
+        </Body>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
